59c6edfeb75fcd817cdf1b0651efe81c964e2966
Guido Stocco <guidostoccodev@gmail.com>
2024-06-20 19:29:23 -0300

    feature valid input

---
+++ Formulario4/js/script.js
@@ -0,0 +1,18 @@
+//Selecionando elementos
+const nameInput = document.querySelector(".cadastro-nome");
+
+
+
+
+
+//function
+function validName(){
+    
+    const nameInputValue = nameInput.value;
+    if(!nameInputValue) return;
+
+}
+
+
+
+//eventos
