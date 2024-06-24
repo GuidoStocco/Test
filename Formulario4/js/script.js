835f6931846ea2b16d4271226cd9eb90573c8804
Guido Stocco <guidostoccodev@gmail.com>
2024-06-23 22:09:27 -0300

    axios

--- Formulario4/js/script.js
+++ Formulario4/js/script.js
@@ -1,18 +1,14 @@
 //Selecionando elementos
-const nameInput = document.querySelector(".cadastro-nome");
 
 
-
-
-
-//function
-function validName(){
-    
-    const nameInputValue = nameInput.value;
-    if(!nameInputValue) return;
-
-}
-
-
-
-//eventos
+$('#formTest').submit(function (e) {
+    e.preventDefault();
+    const form = e.target;
+    const cep = '29930425'; // TODO: fazer lógica de pegar do input
+    axios.get(`https://viacep.com.br/ws/${cep}/json/`)
+        .then((response) => {
+            const { data } = response;
+            console.log(data);
+            $('#city').value = data.localidade
+        })
+})
