f915c07270eb080127205f69e10c4b968ab1bfd9
Guido Stocco <guidostoccodev@gmail.com>
2024-06-25 20:38:15 -0300

    axios.get

--- Formulario4/js/script.js
+++ Formulario4/js/script.js
@@ -18,24 +18,27 @@ const streetInput = document.querySelector("#street");
 const cityInput = document.querySelector("#city");
 const bairroInput = document.querySelector("#neighborhood");
 
+// function validarCep() {
+//     const cep = cepInput.value;
+//     const regexCep = /^[0-9]{8}$/;
 
-function validCep(){
+//     if(regexCep.test(cep)){
+//         return cep;
+//     } else{
+//         throw {Error_cep: 'Invalid'}
+//     };
+// }
+
+
+const get = () => {
 
     const cep = cepInput.value;
-    const regexCep = /^[0-9]{8}$/;
-
-    if(regexCep === cep){
-        axios.get(`https://viacep.com.br/ws/${cep}/json/`)
-    }else{
-        throw {cep_error: 'cep valid'};
-    }
+    axios.get(`viacep.com.br/ws/${cep}/json/`)
+        .then((response) => {
+            console.log(response)
+        })
+ 
 }
-
-cepInput.addEventListener("focusout", () => {
-
-    validCep();
-
-})
 
 
 
